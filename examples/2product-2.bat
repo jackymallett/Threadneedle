48160a855fcbcd090b48308f15268191e8138fcb
Jacky Mallett <warlock@alum.mit.edu>
2016-11-10 17:15:06 +0000

    update

--- examples/2product-2.bat
+++ examples/2product-2.bat
@@ -1,35 +1,35 @@
-load configs/2product-6w.json
+load examples/2product-6w.json
 step 3600
 htmlcharts results/2product-6w "6 workers"
 reset
 #
-load configs/2product-6w.json
+load examples/2product-6w.json
 printmoney Farm-5 200
 printmoney Farm-7 200
 step 3600
 htmlcharts results/2product-6w-200 Farm "6 Workers + 200 for Farm"
 reset
 #
-load configs/2product-6w.json
+load examples/2product-6w.json
 set M-Food payDividend true
 set M-Milk payDividend true
 step 3600
 htmlcharts results/2product-6w-div "6 workers - market dividend"
 reset
 #
-load configs/2productproblem-13.json
+load examples/2productproblem-13.json
 step 3600
 htmlcharts results/13w "13 Workers"
 reset
 #
-load configs/2productproblem-13.json
+load examples/2productproblem-13.json
 printmoney Farm-5 200
 printmoney Farm-7 200
 step 3600
 htmlcharts results/13w-200  "13 Workers +200 @ Farm"
 reset
 #
-load configs/2productproblem-13.json
+load examples/2productproblem-13.json
 step 3600
 printmoney Farm-5 200
 printmoney Farm-7 200
--- examples/2product.bat
+++ examples/2product.bat
@@ -12,13 +12,13 @@ step 3600
 htmlcharts results/default-200 Farm "6 Workers + 200 for Farm"
 reset
 #
-load configs/2productproblem-7.json
+load examples/2productproblem-7.json
 set seed 7272911
 step 3600
 htmlcharts results/7w  "7 Workers"
 reset
 #
-load configs/2productproblem-7.json
+load examples/2productproblem-7.json
 set seed 7272911
 printmoney Farm-5 200
 printmoney Farm-7 200
@@ -26,7 +26,7 @@ step 3600
 htmlcharts results/7w-200 "7 Workers-200"
 reset
 #
-load configs/2productproblem-13.json
+load examples/2productproblem-13.json
 set seed 7272911
 printmoney Farm-5 200
 printmoney Farm-7 200
@@ -34,7 +34,7 @@ step 3600
 htmlcharts results/13w  "13 Workers"
 reset
 #
-load configs/2productproblem-13.json
+load examples/2productproblem-13.json
 set seed 7272911
 step 3600
 htmlcharts results/13w-200 "13 Workers"
